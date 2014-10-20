8e6a55c9de611c9003d435122e457e46045318d2
Glen Baker <iepathos@gmail.com>
2014-10-20 10:54:23 -0700

    Added test for directory check

--- goupdate_test.go
+++ goupdate_test.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
-// func TestIsDir(t *testing.T) {
+func TestIsDir(t *testing.T) {
+	os.Mkdir("tmp", 0777)
 
-// }
+	currentDirectory, err := os.Getwd()
+	if err != nil {
+		t.Error("An error occured trying to get current working directory:", err)
+	}
 
-// func TestParseFilenames(t *testing.T) {
+	if !IsDir(currentDirectory + "/tmp") {
+		t.Error("IsDir returned false for directory tmp")
+	}
 
-// }
+	err2 := os.Remove("tmp")
+	if err2 != nil {
+		t.Error("An error occured trying to remove tmp directory:", err2)
+	}
+}
 
 func TestIsGoFile(t *testing.T) {
 	filename := "test.go"
@@ -18,6 +29,10 @@ func TestIsGoFile(t *testing.T) {
 		t.Error("IsGoFile returned false for filename test.go")
 	}
 }
+
+// func TestParseFilenames(t *testing.T) {
+
+// }
 
 // func TestCheckDirForGo(t *testing.T) {
 
